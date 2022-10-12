56e043d414d219440a5ce000da884737827fc12a
kkywalk2 <kkywalk2@gmail.com>
2022-10-13 01:37:56 +0900

    feat: add LocalDateTimeType

---
+++ kkywalk2/src/main/java/org/weekly/type/LocalDateTimeType.java
@@ -0,0 +1,25 @@
+package org.weekly.type;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.time.LocalDateTime;
+
+public class LocalDateTimeType implements OrmType<LocalDateTime> {
+
+    private final String columnName;
+
+    public LocalDateTimeType(String columnName) {
+        this.columnName = columnName;
+    }
+
+    @Override
+    public int getJdbcType() {
+        return Types.BIGINT;
+    }
+
+    @Override
+    public LocalDateTime getJavaTypeObject(ResultSet rs) throws SQLException {
+        return rs.getObject(columnName, LocalDateTime.class);
+    }
+}
